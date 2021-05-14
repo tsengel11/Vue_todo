fa0cb5e622827e45ccd63a9f974bdb4c9b47d4af
Bayartsengel Baatarkhuyag <bayartsengelt@gmail.com>
2021-05-15 09:04:10 +1000

    aa

--- resources/js/vue/listItem.vue
+++ resources/js/vue/listItem.vue
@@ -4,12 +4,27 @@
          @change="updateCheck()"
          v-model="item.completed"
          />
-         {{item.name}}
+         <span :class="[item.completed ? 'completed':'','itemText']">{{item.name}}</span>
+         <button @click="removeItem()" class="trashcan">remove</button>
     </div>
 </template>
 
 <script>
 export default {
-    props:['item']
+    props:['item'],
+
+    methods:{
+
+    }
 }
 </script>
+<style scoped>
+
+.completed {
+    text-decoration:line-through ;
+}
+.trashcan {
+    color:red
+}
+
+</style>
